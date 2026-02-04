ed50141ce5e212c264168b1a57143fa1235faf20
SENIOR\Ligiane.Vieira <ligiane.vieira@senior.com.br>
2026-02-03 21:48:45 -0300

    meu primeiro commit com git

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,22 @@
+import {test, expect} from "@playwright/test";
+
+test('dev consultar um pedido aprovado', async ({ page }) => {
+
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-R083QG')
+    await page.getByTestId('search-order-button').click()
+
+    // Assert
+    await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
+    await expect(page.getByTestId('order-result-id')).toContainText('VLO-R083QG')
+
+    await expect(page.getByTestId('order-result-status')).toBeVisible()
+    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+})
